a8d0d6f36456ae3c2508dbb2f6fcf5182230d434
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-19 10:13:40 +0100

    Sets up file structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fiveMinCatchupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fiveMinCatchupApp
+ */
+angular.module('fiveMinCatchupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
