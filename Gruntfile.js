09fbc48a009b01fe43b9c21b5877431944c5d0d7
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-20 14:23:00 +0100

    Adjusting for heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -439,6 +439,10 @@ module.exports = function (grunt) {
   grunt.registerTask('default', [
     'newer:jshint',
     'test',
-    'build'
+    'build',
   ]);
+
+  grunt.registerTask('heroku',
+    ['compass:dist', 'autoprefixer', 'imagemin']);
+  
 };
